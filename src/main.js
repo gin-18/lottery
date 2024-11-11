a74156ecf22222af6bc6b700be93adbf4cc46fb7
gin <gin-18@qq.com>
2024-11-11 21:02:14 +0800

    chore: add vercel analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from '@/router/router.js'
 import { vuetify } from '@/plugins/vuetify.js'
+import { inject } from '@vercel/analytics'
 
 const pinia = createPinia()
+
+inject()
 
 createApp(App).use(router).use(pinia).use(vuetify).mount('#app')
