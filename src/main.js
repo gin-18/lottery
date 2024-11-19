5febbd9135b1c56ace56759a66acd09d2454451d
gin <gin-18@qq.com>
2024-11-19 12:41:19 +0800

    chore: add vercel speed-insights

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createPinia } from 'pinia'
 import { router } from '@/router/router.js'
 import { vuetify } from '@/plugins/vuetify.js'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 const pinia = createPinia()
 
 inject()
+injectSpeedInsights()
 
 createApp(App).use(router).use(pinia).use(vuetify).mount('#app')
