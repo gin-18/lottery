e2c94e9fa812b06bb54cb5f701b781e628ba7445
gin <gin-18@qq.com>
2024-12-02 16:16:08 +0800

    refator: result container to show each code result

---
+++ src/components/content/ResultContainer.vue
@@ -0,0 +1,32 @@
+<script setup>
+/**
+ * 这个组件用于渲染一期的开奖结果
+ * 传入一期的数据，渲染开奖期次，开奖日期，开奖号码
+ */
+import { computed } from 'vue'
+import { getBallNum } from '@/assets/js/count'
+import { formatDay } from '@/assets/js/formatDay'
+import Ball from '@/components/content/Ball.vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+})
+
+const ballList = computed(() => getBallNum(props.data))
+</script>
+
+<template>
+  <div class="d-flex flex-column ga-4">
+    <div class="d-flex align-center ga-4 text-text">
+      <p>第{{ data.code }}期</p>
+      <p>{{ formatDay(data.day) }}</p>
+    </div>
+
+    <div class="d-flex align-center ga-4">
+      <Ball v-for="num in ballList" :key="num" :num="num" />
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,11 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { getLatestData } from '@/assets/js/request'
-import { formatDay } from '@/assets/js/formatDay'
-import { getBallNum } from '@/assets/js/count'
 import Header from '@/components/header/Header.vue'
-import Ball from '@/components/content/Ball.vue'
+import ResultContainer from '@/components/content/ResultContainer.vue'
 import RewardTable from '@/components/content/RewardTable.vue'
 
 const data = ref([])
-const ballList = computed(() => getBallNum(data.value))
 
 async function setLatestData() {
   data.value = await getLatestData()
@@ -25,16 +22,7 @@ onMounted(() => {
   <v-container class="text-text">
     <h2 class="text-h6 font-weight-bold pb-2">最新开奖</h2>
     <div v-if="!data.code"></div>
-    <div v-else class="d-flex flex-column ga-2 mb-12 pb-4">
-      <div class="d-flex ga-6 text-text pb-2">
-        <p>第{{ data.code }}期</p>
-        <p>{{ formatDay(data.day) }}</p>
-      </div>
-
-      <div class="d-flex align-center ga-4">
-        <Ball v-for="num in ballList" :key="num" :num="num" />
-      </div>
-    </div>
+    <ResultContainer v-else :data="data" />
 
     <h2 class="text-h6 font-weight-bold pb-6">奖金对照表</h2>
     <RewardTable />
