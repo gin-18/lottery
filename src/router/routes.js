1d9d27acf800e1a55f671308a8571e6eacee82eb
gin <gin-18@qq.com>
2024-12-19 09:44:20 +0800

    refator: title for each page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,31 +2,37 @@ export const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: { title: '快乐8研究所' },
     component: () => import('@/views/Home.vue'),
   },
   {
     path: '/results',
     name: 'Results',
+    meta: { title: '开奖结果' },
     component: () => import('@/views/Results.vue'),
   },
   {
     path: '/number',
     name: 'Number',
+    meta: { title: '号码分析' },
     component: () => import('@/views/NumberAnalyze.vue'),
   },
   {
     path: '/interval',
     name: 'Interval',
+    meta: { title: '区间分析' },
     component: () => import('@/views/IntervalAnalyze.vue'),
   },
   {
     path: '/tail',
     name: 'tail',
+    meta: { title: '尾数分析' },
     component: () => import('@/views/TailAnalyze.vue'),
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
+    meta: { title: '快乐8研究所' },
     component: () => import('@/views/404.vue'),
   },
 ]
