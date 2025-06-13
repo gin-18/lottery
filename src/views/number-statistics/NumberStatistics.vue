c2bf726f62cfd24fa4a68bbfde0851800ec73d69
gin <gin-18@qq.com>
2025-06-13 22:09:13 +0800

    refactor(number-statistics): put CurrentNumberStatistics.vue and CurrentNumberStatisticsSetting.vue in one directory

--- src/views/number-statistics/NumberStatistics.vue
+++ src/views/number-statistics/NumberStatistics.vue
@@ -4,14 +4,14 @@ import { storeToRefs } from 'pinia'
 import { useNumberDataStore } from '@/stores/number_page_data'
 import Header from '@/components/header/Header.vue'
 import LoadingWrapper from '@/components/content/LoadingWrapper.vue'
-import CurrentNumberStatistics from './components/CurrentNumberStatistics.vue'
-import RepeatCount from '@/components/analyze/RepeatCount.vue'
+import CurrentNumberStatistics from './components/current-number-statistics/CurrentNumberStatistics.vue'
+import CurrentNumberStatisticsSetting from './components/current-number-statistics/CurrentNumberStatisticsSetting.vue'
+import RepeatNumberStatistics from './components/RepeatNumberStatistics.vue'
 import TimesNumberCount from '@/components/analyze/number/TimesNumberCount.vue'
 import FrequencyCount from '@/components/analyze/number/FrequencyCount.vue'
 import CurrentOmissionCount from '@/components/analyze/number/CurrentOmissionCount.vue'
 import NumberDistribution from '@/components/analyze/number/NumberDistribution.vue'
 import SettingBox from '@/components/content/SettingBox.vue'
-import CurrentResultSetting from '@/components/setting/CurrentResultSetting.vue'
 import RepeatCountSetting from '@/components/setting/RepeatCountSetting.vue'
 import TimesNumberCountSetting from '@/components/setting/TimesNumberCountSetting.vue'
 import FrequencyCountSetting from '@/components/setting/FrequencyCountSetting.vue'
@@ -43,7 +43,7 @@ provide('rawDataArray', rawDataArray)
     <section>
       <h2>重号统计</h2>
       <LoadingWrapper :is-loading="isLoading">
-        <RepeatCount />
+        <RepeatNumberStatistics />
       </LoadingWrapper>
     </section>
 
@@ -76,7 +76,7 @@ provide('rawDataArray', rawDataArray)
     </section>
 
     <SettingBox title="号码分析设置">
-      <CurrentResultSetting />
+      <CurrentNumberStatisticsSetting />
       <RepeatCountSetting />
       <TimesNumberCountSetting />
       <FrequencyCountSetting />
